7fc2c40907cde42ffe4b79388cf03fab20cb8ac3
riku-1125 <24884rikuo@gmail.com>
2026-09-16 18:58:38 +0900

    クッキーの保存、削除の実装

---
+++ chap10/10-4/cookie_values/delete_cookie.php
@@ -0,0 +1,24 @@
+<?php
+$result = setcookie("message", "", time() - 3600);
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>クッキー削除ページ</title>
+    <link href="../../css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div>
+<?php
+if ($result) {
+    echo "クッキーを削除します。", "<hr>";
+    echo '<a href="check_cookie.php">クッキーを確認するページへ</a>';
+} else {
+    echo '<span class="error">クッキーの削除でエラーがありました。</span>', "<br>";
+}
+?>
+    </div>
+  </body>
+</html>
